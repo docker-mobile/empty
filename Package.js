f58c54e667d4bb18a4c744e0be534e1defbfed2e
docker-mobile <143889618+docker-mobile@users.noreply.github.com>
2024-11-09 18:53:51 +0330

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,17 @@
+{
+  "name": "tmate-server",
+  "version": "1.0.0",
+  "description": "A simple HTTP server to download and run tmate",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "https": "^1.0.0"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  },
+  "author": "The almighty dexapy",
+  "license": "MIT"
+    }
